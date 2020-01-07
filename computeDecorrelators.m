5228c7d347ca98681246ebb407aef8060ce4b8e0
Michael Hirschmugl <michael.hirschmugl@gmx.at>
2020-01-07 21:25:07 +0100

    initialization implemented

---
+++ computeDecorrelators.m
@@ -0,0 +1,121 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [decorFilt, decorDelay] = computeDecorrelators(nOutChan, fs)
+% calls function that designs FIR filters for decorrelation
+%decorFilt = compute_delay_decorrelation_response(fs,nOutChan);
+decorDelay = 1500;
+
+%This script creates decorrelation filters that have randomly
+% delayed impulses at different frequency bands.
+order = 3000;       %order of the bandpass filters
+len = 1024*8;       %length of the decorrelation filters
+maxdel = 80;        %maximum delay of the filters
+mindel = 3;         %minimum delay of the filters
+minmaxlocal = 30;   %above 1500Hz the value for delay upper limit
+maxminlocal = 20;   %below 1500Hz the value for delay upper limit
+mincycles = 10;     %mininum amount of delay in cycles
+maxcycles = 40;     %maximum amount of delay in cycles
+
+%compute the values in samples
+maxdelN = round(maxdel/1000*fs);
+mindelN = round(mindel/1000*fs);
+minmaxlocalN = round(minmaxlocal/1000*fs);
+maxminlocalN = round(maxminlocal/1000*fs);
+if maxdelN > len-(order+1)
+    maxdelN = len-(order+1);
+end
+if minmaxlocalN > maxdelN
+    minmaxlocalN = maxdelN;
+end
+% Compute frequency band
+[fpart, npart] = makepart_constcut(200, 2, nOutChan);
+cutoff_f = fpart;
+cutoff = cutoff_f/fs*2;
+cycleN = fs./cutoff_f;
+%compute the bandpass filters
+h = zeros(order+1,npart,nOutChan);
+for j = 1:nOutChan
+    h(:,1,j) = fir1(order, cutoff(1,j),'low');
+    for i = 2:npart
+        h(:,i,j) = fir1(order, [cutoff(i-1,j) cutoff(i,j)], 'bandpass');
+    end
+end
+% Compute the maximum and minimum delays
+curveon = ones(npart,1);
+mindellocalN = zeros(npart,1);
+maxdellocalN = zeros(npart,1);
+for i = 1:npart
+    maxdellocalN(i) = round(maxcycles*(1/cutoff_f(i))*fs);
+    mindellocalN(i) = round(mincycles*(1/cutoff_f(i))*fs);
+    if maxdellocalN(i) > maxdelN
+        maxdellocalN(i) = maxdelN;
+    end
+    if maxdellocalN(i) < minmaxlocalN
+        maxdellocalN(i) = minmaxlocalN;
+        curveon(i) = 0;
+    end
+    if mindellocalN(i) < mindelN
+        mindellocalN(i) = mindelN;
+    end
+    if mindellocalN(i) > maxminlocalN
+        mindellocalN(i) = maxminlocalN;
+    end
+end
+%convert to samples
+maxdellocal = maxdellocalN/fs*1000;
+mindellocal = mindellocalN/fs*1000;
+delvariation = maxdellocal - mindellocal;
+cycleT = cycleN/fs*1000;
+%randomize the delays of the first band
+decorFilt = zeros(len,nOutChan);
+delayinit = (maxdelN-mindellocalN(1))*rand(1,nOutChan)+mindellocalN(1);
+delay(1,:) = round(delayinit);
+% Compute the frequency-dependent delay curve for each loudspeaker channel.
+% A heuristic approach is used to form the curve, which limits how
+% the delay varies between adjacent frequency channels.
+for m = 1:nOutChan
+    for i = 2:npart
+        cycles = 0.5*i*i+1;
+        if curveon(i) == 0
+            delchange = cycleN(i-1,m)*(round(rand(1,1)*cycles*2-cycles));
+        else
+            delchange = cycleN(i-1,m)*(round(rand(1,1)*cycles*2-1.3*cycles));
+        end
+        delay(i,m) = delay(i-1,m) + delchange;
+        if delay(i,m) < mindellocalN(i)
+            k = 0;
+            while delay(i,m) < mindellocalN(i)
+                delay(i,m) = delay(i,m) + cycleN(i-1,m);
+                k = k+1;
+            end
+            if curveon(i) == 0
+                delay(i,m) = delay(i,m) + round(k/2)*cycleN(i-1,m);
+            end
+            while delay(i,m) > maxdellocalN(i)
+                delay(i,m) = delay(i,m) - cycleN(i-1,m);
+            end
+        elseif delay(i,m) > maxdellocalN(i)
+            k = 0;
+            while delay(i,m) > maxdellocalN(i)
+                delay(i,m) = delay(i,m) - cycleN(i-1,m);
+                k = k+1;
+            end
+            if curveon(i) == 0
+                delay(i,m) = delay(i,m) - round(k/2)*cycleN(i-1,m);
+            end
+            while delay(i,m) < mindellocalN(i)
+                delay(i,m) = delay(i,m) + cycleN(i-1,m);
+            end
+        end
+        delay(i,m) = round(delay(i,m));
+    end
+    
+    % Summing up the response from band-pass impulse responses
+    hdelayed = zeros(len,npart);
+    for i = 1:npart
+        hdelayed(delay(i,m)+1:delay(i,m)+order+1,i) = h(:,i,m);
+    end
+    for i = 1:npart
+        decorFilt(:,m) = decorFilt(:,m) + hdelayed(:,i);
+    end
+end
+end
--- dirac.m
+++ dirac.m
@@ -9,38 +9,38 @@
 % Licensed under GNU General Public License v2.0
 %
 % Missing functions:
-% - DirAC_init_stft()
 % - DirAC_run_stft()
 % - Headphone_ITDILD_simulation()
 %
 % Other ToDo:
-% - Change processing to 6 speaker setup
-% - implement HRTF simulation
+% - Change processing to 6 (4?) speaker setup
+% - implement HRTF simulation (done in Reaper)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all
 clc
 
-load pkg signal
+pkg load signal
 
 fs = 48000;
 siglen = 12 * fs;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Only generating signals for test
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Sawtooth signals with repeated exp-decaying temporal envelope
 sig1 = (mod([1:siglen]',200)/200-0.5) .* (10.^((mod([siglen:-1:1]',fs/5)/(fs/10)))-1)/10;
-sig2 = (mod([1:siglen]',321)/321-0.5) .* (10.^((mod([siglen:-1:1]',fs/2)/(fs/ 4)))-1)/10;
 
 % Simulate B-format signals for the sources 
 azi1 = [1:siglen]' / siglen*3*360;					% changing source azimuth for sig1
 ele1 = [1:siglen]' * 0;								% constant elevation for sig1
-azi2 = round([1:siglen]' / siglen) * 180 - 90;		% azi for sig2
-ele2 = [1:siglen/2 siglen/2:-1:1]' / siglen * 180;	% changing elev for sig2
 
-bw = (sig1+sig2) / sqrt(2);
-bx = sig1 .* cos(azi1/180*pi) .* cos(ele1/180) + sig2 .* cos(azi2/180*pi) .* cos(ele2/180*pi); 
-by = sig1 .* sin(azi1/180*pi) .* cos(ele1/180) + sig2 .* sin(azi2/180*pi) .* cos(ele2/180*pi);
-bz = sig1 .* sin(ele1/180*pi) + sig2 .* sin(ele2/180*pi);
+bw = (sig1) / sqrt(2);
+bx = sig1 .* cos(azi1/180*pi) .* cos(ele1/180); 
+by = sig1 .* sin(azi1/180*pi) .* cos(ele1/180);
+bz = sig1 .* sin(ele1/180*pi);
 
 % Add fading in diffuse low-passed noise about evenly in 3D 
 [b,a] = butter(1, [500/fs/2]); 
@@ -60,11 +60,27 @@ bfsig_2D = [bw bx by bz*0];
 bfsig_2D = bfsig_2D / max(max(abs(bfsig_2D))) / 3;
 bfsig_2D(end-500:end,:) = bfsig_2D(end-500:end, :) .* (linspace(1, 0, 501)' * [1 1 1 1]);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The generated sound will circle three times in azimut and have diffuse sound on all
+% channels fading in.
+% There is no elevation modulation.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Writing 4 channel audio file to disk for testing
+audiowrite(['Output2D-B-Format.wav'], bfsig_2D, fs);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is where the speakers are defined and upmixing happens.
+% We define 4 speakers for minimum 2D FOA
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Define the directions of loudspeakers 
-ls_dirs_2D = [-30 0 30 -110 110 -150 150]';			% 7.1 surround
+ls_dirs_2D = [0 90 180 270]';
 
 %Initialize DirAC parameters
-%DirAC_struct = DirAC_init_stft(ls_dirs_2D, fs);
+DirAC_struct = DirAC_init_stft(ls_dirs_2D, fs);
 
 % DirAC processing
 %[LSsig_2D, DIRsig_2D, DIFFsig_2D, DirAC_struct] = DirAC_run_stft(bfsig_2D, DirAC_struct);
@@ -72,3 +88,5 @@ ls_dirs_2D = [-30 0 30 -110 110 -150 150]';			% 7.1 surround
 % Simple simulation of LS listening with headphones
 %HPsig_LIN2D   = Headphone_ITDILD_simulation(LINsig_2D, DirAC_struct);
 %HPsig_DirAC2D = Headphone_ITDILD_simulation(LSsig_2D, DirAC_struct);
+
+%audiowrite(['Output2D-LIN.wav'], LINsig_2D, fs);
