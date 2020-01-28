c215f13462b1925aefabc966973e39c929ab51e3
Hirschmensch <michael.hirschmugl@gmx.at>
2020-01-28 19:29:29 +0100

    delete all

--- updateDiffuseFilters.m
+++
@@ -1,8 +0,0 @@
-function [diffuseFilterspec, DirAC_struct] = updateDiffuseFilters(pars, DirAC_struct)
- 
-    diff = pars(:,4);
-    % Combine separation filters with approximate correction for the
-    % effect of virtual microphones to the diffuse sound energy, 
-    % and energy weights per loudspeaker
-    diffuseFilterspec = sqrt(diff) * (DirAC_struct.diffCorrection.*DirAC_struct.lsDiffCoeff);
-end
