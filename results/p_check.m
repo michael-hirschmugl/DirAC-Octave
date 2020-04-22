44e291fc58f5096913cc1ceb2833e4c74b58546a
--- <mplanton@pluto.tu-graz.ac.at>
2020-04-22 15:47:06 +0200

    p-value and correlation

--- results/p_check.m
+++ results/p_check.m
@@ -1,4 +1,4 @@
-pkg load statistics
+%pkg load statistics
 
 clear
 close all
@@ -15,4 +15,18 @@ x = [1:length(Klang)];
 %hist(Raum(:,7))
 %qqplot(x, Klang(:,7), '-', x)
 
-ttest(Klang(:,1), Raum(:,1))
+%[h_1, p_1] = ttest(Klang(:,1), Raum(:,1))
+
+for i = [1:7]
+    [h(i), p(i)] = ttest(Klang(:,i), Raum(:,i));
+    R = corrcoef(Klang(:,i), Raum(:,i));
+    r(i) = R(1, 2);
+end
+
+figure
+plot(1:length(p), p, 'o', 1:length(r), r, 'x')
+set(gca,'xtick',1:7,'xticklabel',{'LSDecorr','LSFDN','TdesFDN','TdesWid','HARPEX','COMPASS','FOA'})
+legend('p-Wert', 'Korrelationskoeffizient')
+
+xlim([0.5 7.5])
+ylim([-0.05 1.05])
